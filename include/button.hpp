d5d7ef74e9229a02c9990812a545d0533a6f7a86
noah.bloom <noah.bloom@everactive.com>
2021-01-10 23:08:41 -0500

    updated symlinks

---
+++ include/button.hpp
@@ -0,0 +1,31 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+// symlink from appropriate location
+// possibly $HOME/.arduino15/packages/arduino/hardware/samd/x.y.z
+#include "cores/arduino/api/ArduinoAPI.h"
+
+/* basic class for controlling and debouncing buttons */
+namespace components {
+class Button {
+   private:
+    int m_pin;
+    unsigned long m_msDelay;
+    unsigned long m_lastDebounce;
+    bool m_high;
+    bool m_read = LOW;
+
+   public:
+    Button(int pin, unsigned long msDelay, bool high) {
+        m_pin = pin;
+        m_msDelay = msDelay;
+        m_high = high;
+
+        m_lastDebounce = 0;
+        pinMode(m_pin, INPUT);
+    };
+    bool isPressed();
+};
+};  // namespace components
+
+#endif
