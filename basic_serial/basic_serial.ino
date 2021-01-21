01ea67de04824498a5ac97527181dff233e0613b
noah.bloom <noah.bloom@everactive.com>
2021-01-20 22:15:47 -0500

    printing unsigned char vals

--- basic_serial/basic_serial.ino
+++ basic_serial/basic_serial.ino
@@ -1,4 +1,6 @@
-void printMe() {
+
+/* one way to print 0,1,2 to serial */
+void printUint8() {
     u_int8_t* val = new u_int8_t[3];
     val[0] = 01;
     val[1] = 02;
@@ -7,9 +9,22 @@ void printMe() {
         Serial.println(val[j]);
     }
 }
+
+/* or use unsigned char and bitwise... */
+void printUchar() {
+    int i = 1;
+    unsigned char val[] = {
+        i >> 1,  // should be 0
+        i,       // should be 1
+        i << 1   // should be 2
+    };
+    for (int i = 0; i < 3; i++) {
+        Serial.println(val[i]);
+    }
+}
 void setup() {}
 
 void loop() {
-    printMe();
+    printUchar();
     delay(5000);
 }
